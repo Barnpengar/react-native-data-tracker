12e5b02ef78824b5ff7de72c94b6a5871606fce3
tmaas510 <tmaas510@yahoo.com>
2016-04-01 10:43:34 +0200

    update RNDataTrackerIOS.js file

--- RNDataTrackerIOS.js
+++ RNDataTrackerIOS.js
@@ -4,11 +4,11 @@ var { NativeModules } = require('react-native');
 var Promise = require('bluebird'); // jshint ignore:line
 var DataTrackers = NativeModules.RNDataTrackerIOS;
 
-var _getDataIn = Promise.promisify(DataTracker.getDataIn);
-var _getDataOut = Promise.promisify(DataTracker.getDataOut);
+var _getDataIn = Promise.promisify(DataTrackers.getDataIn);
+var _getDataOut = Promise.promisify(DataTrackers.getDataOut);
 
-var _getWifiIn = Promise.promisify(DataTracker.getWifiIn);
-var _getWifiOut = Promise.promisify(DataTracker.getWifiOut);
+var _getWifiIn = Promise.promisify(DataTrackers.getWifiIn);
+var _getWifiOut = Promise.promisify(DataTrackers.getWifiOut);
 
 var DataTracker = {
     getDataIn() {
