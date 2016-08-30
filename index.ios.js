20a2c03370bb044bce6a058e9129909d0bb7b4cc
anback <anders.back@me.com>
2016-08-30 03:30:48 +0200

    first look at android support

---
+++ index.ios.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var { NativeModules } = require('react-native');
+var DataTrackers = NativeModules.RNDataTrackerIOS;
+const promisify = require("es6-promisify");
+
+var _getDataIn = promisify(DataTrackers.getDataIn);
+var _getDataOut = promisify(DataTrackers.getDataOut);
+
+var _getWifiIn = promisify(DataTrackers.getWifiIn);
+var _getWifiOut = promisify(DataTrackers.getWifiOut);
+
+var DataTracker = {
+    getDataIn() {
+        return _getDataIn()
+    },
+    getDataOut() {
+        return _getDataOut()
+    },
+    getWifiIn() {
+        return _getWifiIn()
+    },
+    getWifiOut() {
+        return _getWifiOut()
+    }
+};
+
+module.exports = DataTracker
