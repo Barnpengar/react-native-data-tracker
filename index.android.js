31a60672c29b35f3cd3e4dbe10bf39ec8e1b37bc
Anders Back <anders.back@me.com>
2016-08-30 16:07:29 +0200

    removed ese6 promise

--- index.android.js
+++ index.android.js
@@ -1,28 +1,13 @@
 'use strict';
 
-var { NativeModules } = require('react-native');
-var DataTrackers = NativeModules.RNDataTracker;
-const promisify = require("es6-promisify");
-
-var _getDataIn = promisify(DataTrackers.getDataIn);
-var _getDataOut = promisify(DataTrackers.getDataOut);
-
-var _getWifiIn = promisify(DataTrackers.getWifiIn);
-var _getWifiOut = promisify(DataTrackers.getWifiOut);
+var { NativeModules } = require('react-native')
+var DataTrackers = NativeModules.RNDataTracker
 
 var DataTracker = {
-    getDataIn() {
-        return _getDataIn()
-    },
-    getDataOut() {
-        return _getDataOut()
-    },
-    getWifiIn() {
-        return _getWifiIn()
-    },
-    getWifiOut() {
-        return _getWifiOut()
-    }
+    getDataIn : DataTrackers.getDataIn,
+    getDataOut : DataTrackers.getDataOut,
+    getWifiIn : DataTrackers.getWifiIn,
+    getWifiOut : DataTrackers.getWifiOut
 };
 
 module.exports = DataTracker
