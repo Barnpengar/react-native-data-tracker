406da838f65bd5bb12888d63fa590253b8ccc291
Anders Back <anders.back@me.com>
2016-08-30 16:08:18 +0200

    Promise istead of callbacks

--- android/src/main/java/com/gimitheapp/RNDataTracker/RNDataTrackerModule.java
+++ android/src/main/java/com/gimitheapp/RNDataTracker/RNDataTrackerModule.java
@@ -2,7 +2,7 @@ package com.gimitheapp.RNDataTracker;
 
 import android.app.Activity;
 import android.net.TrafficStats;
-import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
@@ -13,44 +13,44 @@ public class RNDataTrackerModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void getDataIn(Callback successCallback, Callback cancelCallback) {
+    public void getDataIn(Promise promise) {
         Activity currentActivity = getCurrentActivity();
 
-        if (currentActivity == null) {cancelCallback.invoke("Activity doesn't exist"); return;}
-        if (TrafficStats.getMobileRxBytes() == TrafficStats.UNSUPPORTED) {cancelCallback.invoke("TrafficStats is not supported"); return;}
+        if (currentActivity == null) {promise.reject("Activity doesn't exist"); return;}
+        if (TrafficStats.getMobileRxBytes() == TrafficStats.UNSUPPORTED) {promise.reject("TrafficStats is not supported"); return;}
 
-        successCallback.invoke(TrafficStats.getMobileRxBytes());
+        promise.resolve((int)(long) TrafficStats.getMobileRxBytes());
     }
 
     @ReactMethod
-    public void getDataOut(Callback successCallback, Callback cancelCallback) {
+    public void getDataOut(Promise promise) {
         Activity currentActivity = getCurrentActivity();
 
-        if (currentActivity == null) {cancelCallback.invoke("Activity doesn't exist"); return;}
-        if (TrafficStats.getMobileTxBytes() == TrafficStats.UNSUPPORTED) {cancelCallback.invoke("TrafficStats is not supported"); return;}
+        if (currentActivity == null) {promise.reject("Activity doesn't exist"); return;}
+        if (TrafficStats.getMobileTxBytes() == TrafficStats.UNSUPPORTED) {promise.reject("TrafficStats is not supported"); return;}
 
-        successCallback.invoke(TrafficStats.getMobileTxBytes());
+        promise.resolve((int) (long) TrafficStats.getMobileTxBytes());
     }
 
     @ReactMethod
-    public void getWifiIn(Callback successCallback, Callback cancelCallback) {
+    public void getWifiIn(Promise promise) {
         Activity currentActivity = getCurrentActivity();
 
-        if (currentActivity == null) {cancelCallback.invoke("Activity doesn't exist"); return;}
-        if (TrafficStats.getTotalRxBytes() == TrafficStats.UNSUPPORTED) {cancelCallback.invoke("TrafficStats is not supported"); return;}
+        if (currentActivity == null) {promise.reject("Activity doesn't exist"); return;}
+        if (TrafficStats.getTotalRxBytes() == TrafficStats.UNSUPPORTED) {promise.reject("TrafficStats is not supported"); return;}
 
-        successCallback.invoke(TrafficStats.getTotalRxBytes() - TrafficStats.getMobileRxBytes());
+        promise.resolve((int) (long) (TrafficStats.getTotalRxBytes() - TrafficStats.getMobileRxBytes()));
     }
 
 
     @ReactMethod
-    public void getWifiOut(Callback successCallback, Callback cancelCallback) {
+    public void getWifiOut(Promise promise) {
         Activity currentActivity = getCurrentActivity();
 
-        if (currentActivity == null) {cancelCallback.invoke("Activity doesn't exist"); return;}
-        if (TrafficStats.getTotalTxBytes() == TrafficStats.UNSUPPORTED) {cancelCallback.invoke("TrafficStats is not supported"); return;}
+        if (currentActivity == null) {promise.reject("Activity doesn't exist"); return;}
+        if (TrafficStats.getTotalTxBytes() == TrafficStats.UNSUPPORTED) {promise.reject("TrafficStats is not supported"); return;}
 
-        successCallback.invoke(TrafficStats.getTotalTxBytes() - TrafficStats.getMobileRxBytes());
+        promise.resolve((int) (long) (TrafficStats.getTotalTxBytes() - TrafficStats.getMobileRxBytes()));
     }
 
 
