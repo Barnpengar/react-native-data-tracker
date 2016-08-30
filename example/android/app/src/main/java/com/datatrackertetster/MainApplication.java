6ae9ab867d8e683a005dcc594f1a64278431a417
anback <anders.back@me.com>
2016-08-30 05:41:22 +0200

    Build was successful

--- example/android/app/src/main/java/com/datatrackertetster/MainApplication.java
+++ example/android/app/src/main/java/com/datatrackertetster/MainApplication.java
@@ -8,24 +8,26 @@ import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
+import com.gimitheapp.RNDataTracker.RNDataTracker;
 
-import java.util.Arrays;
-import java.util.List;
+    import java.util.Arrays;
+    import java.util.List;
 
-public class MainApplication extends Application implements ReactApplication {
+    public class MainApplication extends Application implements ReactApplication {
 
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-    @Override
-    protected boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
-    }
+      private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+        @Override
+        protected boolean getUseDeveloperSupport() {
+          return BuildConfig.DEBUG;
+        }
 
-    @Override
-    protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
-      );
-    }
+        @Override
+        protected List<ReactPackage> getPackages() {
+          return Arrays.<ReactPackage>asList(
+                  new MainReactPackage(),
+                  new RNDataTracker()
+          );
+        }
   };
 
   @Override
